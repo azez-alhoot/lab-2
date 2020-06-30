ad223863c376e995db994d2796092f3286e44eb2
azez-alhoot <azezalhoot82@yahoo.com>
2020-07-01 01:43:09 +0300

    lab3 done

--- js/app.js
+++ js/app.js
@@ -1,57 +1,165 @@
-"use strict";
+"use strict"
+
+let secondArr = [];
+let arr = [];
+let newArr = [];
+let opNew = [];
+let sortHorn = [];
+let sortText = [];
+
 const getAjax = {
   method: "get",
   datatype: "json",
 };
-$.ajax("./data/page-1.json").then((data) => {
-  data.forEach((val, idx) => {
-    let animal = new Animals(val.title,val.image_url,val.description,val.keyword);
-    animal.renderCard();
-    animal.renderOptions();
-  });
-  $("main div:nth-child(2)").remove();
-  console.log(newArr);
-});
-let arr = [];
-function Animals(title, image_url, description, keyword) {
+// constructor of products 
+function Product(title, image_url, description, keyword, horns) {
   this.title = title;
   this.image_url = image_url;
   this.description = description;
   this.keyword = keyword;
+  this.horns = horns;
   arr.push(this);
 }
-Animals.prototype.renderCard = function(){
-  let imgSection = $("#photo-template").clone();
-  let renH2 = $("#titleId").text(this.title);
-  let renImg = $("#imgId").attr('src',this.image_url);
-  let renParagraph = $('#paraId').text(this.description);
-    $('main').append(imgSection);
-  }
-  var newArr = [];
-  Animals.prototype.renderOptions = function(){
-  if(!newArr.includes(this.keyword)){
+// function to sort the array based on text letters
+function sortByText() {
+  $.ajax("./data/page-1.json").then((data) => {
+    data.forEach((val, idx) => {
+      sortText.push(val);
+    });
+    // console.log(data);
+    data.sort((a,b) => {
+      if (a.title.toUpperCase() > b.title.toUpperCase()){
+        return 1;
+      } else if (a.title.toUpperCase() < b.title.toUpperCase()){
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    sortText = data;
+    console.log(sortText);
+  });
+}
+// function to sort the array based on the horns
+function sortByHorns() {
+  $.ajax("./data/page-1.json").then((data) => {
+    data.forEach((val, idx) => {
+      sortHorn.push(val);
+    });
+    // console.log(data);
+    data.sort((a,b) => {
+      if (a.horns < b.horns){
+        return 1;
+      } else if (a.horns > b.horns){
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    sortHorn = data;
+    console.log(sortHorn);
+  });
+}
+// method to render the card products
+Product.prototype.renderCard = function () {
+  let musTemplate = $('#animalContainer').html();
+  let newProduct = Mustache.render(musTemplate, this);
+  $('main').append(newProduct);
+  return newProduct;
+}
+
+// function to load page one
+function getPageOne() {
+  $.ajax("./data/page-1.json").then((data) => {
+    arr = []
+    data.forEach((val, idx) => {
+      let animal = new Product(val.title, val.image_url, val.description, val.keyword, val.horns);
+      animal.renderCard();
+      animal.renderOptions();
+    });
+  });
+}
+// function to load page tow
+function getPageTow() {
+  arr = [];
+  $.ajax("./data/page-2.json").then((data) => {
+    data.forEach((val, idx) => {
+      let obJsecond = new Product(val.title, val.image_url, val.description, val.keyword, val.horns);
+      obJsecond.renderCard();
+      if (!newArr.includes(val.keyword)) {
+        opNew.push(val.keyword);
+      }
+    });
+  });
+}
+
+// method to render th option list
+Product.prototype.renderOptions = function () {
+  if (!newArr.includes(this.keyword)) {
     newArr.push(this.keyword);
     $('#select1').append(`<option id = "${this.keyword}" value = "${this.keyword}" >${this.keyword}</option>`);
   }
+  // console.log(newArr);
 }
+// event to filter the result based on the title
 $('#select1').on("change", (evnt) => {
   $('main').empty();
   let md = "";
   $("#select1 option:selected").each(function () {
-      md = $(this).text();
+    md = $(this).text();
   });
-  if(md==='Filter by Keyword'){
-      location.reload();
+  if (md === 'Filter by Keyword') {
+    location.reload();
   }
   arr.forEach((item, i) => {
-      let type = item.keyword;
-      if (md===type) {
-          let div = $("<div id ='photo-template'></div>");
-          let h2 = $("<h2 id ='titleId'></h2>").text(item.title);
-          let imgEl = $("<img id = 'imgId'>").attr(`src`, item.image_url);
-          let pEl = $("<p id = 'paraId'></p>").text(item.description);
-          $('main').append(div);
-          div.append(h2, imgEl, pEl);
-      }
+    if (md === item.keyword) {
+      item.renderCard();
+    }
   })
 });
+// event to render page tow
+$('#page2').click(() => {
+  $('main').html('');
+  $('#select1').html('');
+  getPageTow();
+  $('#select1').append(`<option value="default">Filter by Keyword</option>`);
+  opNew.forEach((val, idx) => {
+    $('#select1').append(`<option id = "${val}" value = "${val}" >${val}</option>`);
+  });
+  console.log(opNew);
+  opNew = [];
+});
+// event to render page one
+$('#page1').click(() => {
+  $('main').html('');
+  $('#select1').html('');
+  getPageOne();
+  $('#select1').append(`<option value="default">Filter by Keyword</option>`);
+  newArr.forEach((val, idx) => {
+    $('#select1').append(`<option id = "${val}" value = "${val}" >${val}</option>`);
+  });
+  console.log(newArr);
+});
+// event to sort the page based on the the text
+$('#sortText').click(() => {
+  $('main').html('');
+  sortText.forEach((val, idx) => {
+    let obJ = new Product(val.title, val.image_url, val.description, val.keyword, val.horns);
+    obJ.renderCard();
+  });
+});
+// event to sort the page based on the the horns
+$('#sortHorns').click(() => {
+  $('main').html('');
+  sortHorn.forEach((val, idx) => {
+    let obJ = new Product(val.title, val.image_url, val.description, val.keyword, val.horns);
+    obJ.renderCard();
+  });
+});
+
+// console.log(arr);
+sortByText();
+sortByHorns();
+// console.log(arr);
+
+getPageOne();
